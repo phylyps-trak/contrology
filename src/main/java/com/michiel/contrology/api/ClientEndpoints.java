92a774d144aa6ebe0ec542fa460283da91b05bbd
Michiel <j.michiel.janssens@gmail.com>
2020-06-11 22:48:21 +0200

    1

--- src/main/java/com/michiel/contrology/api/ClientEndpoints.java
+++ src/main/java/com/michiel/contrology/api/ClientEndpoints.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/clients")
 public class ClientEndpoints
+    //KIJK MAAR DIT IS DE NIEUWSTE
 {
     @Autowired
     ClientService clientService;
